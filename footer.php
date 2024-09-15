e2924ac898789cc5bec103e38e2b32ae5e7c8f35
Emran Hossen <emran@Muzahids-MacBook-Pro.local>
2024-09-15 19:17:16 +0600

    first commit

---
+++ footer.php
@@ -0,0 +1,42 @@
+<footer>
+  <div class="footer_inside">
+    <div class="footer_item">
+      <img class="footer_logo_img" src="img/88.jpg" />
+    </div>
+    <div class="footer_item">
+      <h2 class="footer_heading">CONTACT INFORMATION</h2>
+
+      <p class="footer_item_text">Briere Property Management</p>
+      <p class="footer_item_text">
+        Direct: <a href="tel:+440000">403-465-0404</a>
+      </p>
+      <p class="footer_item_text">
+        Email: <a href="mail:david@brierepm.com">david@brierepm.com</a>
+      </p>
+    </div>
+    <div class="footer_item">
+      <h2 class="footer_heading">CONTACT INFORMATION</h2>
+
+      <p class="footer_item_text">8A - 4550 112 Avenue SE</p>
+      <p class="footer_item_text">Calgary, Alberta,</p>
+      <p class="footer_item_text">T2C 2K2</p>
+    </div>
+    <div class="footer_item">
+      <div class="footer_socials_keeper">
+        <a class="footer_social_item" href="https://facebook.com">
+          <i class="bi bi-facebook"></i>
+        </a>
+        <a class="footer_social_item" href="#">
+          <i class="bi bi-linkedin"></i>
+        </a>
+        <a class="footer_social_item" href="#">
+          <i class="bi bi-instagram"></i>
+        </a>
+        <a class="footer_social_item" href="#">
+          <i class="bi bi-twitter"></i>
+        </a>
+      </div>
+    </div>
+  </div>
+</footer>
+
---
+++ index.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+    />
+  </head>
+
+  <body>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
+        <a href="" class="navbar-logo">LOGO</a>
+
+        <button
+          class="navbar-toggler"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarId"
+        >
+          <img src="img/list.svg" alt="" />
+        </button>
+        <div class="collapse navbar-collapse" id="navbarId">
+          <ul class="navbar-nav ms-auto">
+            <li class="navbar-menu"><a href="" class="nav-link">Home</a></li>
+            <li class="navbar-menu"><a href="" class="nav-link">About</a></li>
+            <li class="navbar-menu">
+              <a href="" class="nav-link">Services</a>
+            </li>
+            <li class="navbar-menu"><a href="" class="nav-link">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <section class="section-1 position-relative">
+      <div class="container position-absolute top-50 start-50 translate-middle">
+        <h3 class=" ">
+          A Team That Delivers the Highest Standard of Quality Service
+        </h3>
+      </div>
+    </section>
+
+    <section class="teamSection">
+      <div class="mainTeamSection">
+        <h2>The Briere Property Management Team</h2>
+
+        <div class="container">
+          <div class="row">
+            <div class="col-4 teamImg">
+              <img src="img/Briere-TeamShot.jpg" alt="" />
+            </div>
+            <div class="col-8 position-relative imgText">
+              <p
+                class="imgText position-absolute top-50 start-50 translate-middle"
+              >
+                With our full service Briere Property Management, you get much
+                more than just one over-worked Formed in 2012, you have a
+                combined 20+ years experience working for you.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="memberSection">
+      <div class="row m-5">
+        <div class="col-sm-6 teamCard">
+          <div class="card border-white">
+            <div class="cardImg position-relative">
+              <img src="img/DavidBriere.png" alt="" />
+            </div>
+            <div
+              class="card-body position-absolute top-50 start-50 translate-middle"
+            >
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 teamCard">
+          <div class="card border-white">
+            <div class="cardImg position-relative">
+              <img src="img/TrudyElder.png" alt="" />
+            </div>
+            <div
+              class="card-body position-absolute top-50 start-50 translate-middle"
+            >
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row m-5">
+        <div class="col-sm-6 teamCard">
+          <div class="card border-white">
+            <div class="cardImg position-relative">
+              <img src="img/DavidBriere.png" alt="" />
+            </div>
+            <div
+              class="card-body position-absolute top-50 start-50 translate-middle"
+            >
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 teamCard">
+          <div class="card border-white">
+            <div class="cardImg position-relative">
+              <img src="img/TrudyElder.png" alt="" />
+            </div>
+            <div
+              class="card-body position-absolute top-50 start-50 translate-middle"
+            >
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row m-5">
+        <div class="col-sm-6 teamCard">
+          <div class="card border-white">
+            <div class="cardImg position-relative">
+              <img src="img/DavidBriere.png" alt="" />
+            </div>
+            <div
+              class="card-body position-absolute top-50 start-50 translate-middle"
+            >
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 teamCard">
+          <div class="card border-white">
+            <div class="cardImg position-relative">
+              <img src="img/TrudyElder.png" alt="" />
+            </div>
+            <div
+              class="card-body position-absolute top-50 start-50 translate-middle"
+            >
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="reviewSec">
+      <div
+        id="carouselExampleCaptions"
+        class="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              src="img/about-hero.jpg"
+              class="d-block w-100 h-50"
+              alt="..."
+            />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img
+              src="img/about-hero.jpg"
+              class="d-block w-100 h-50"
+              alt="..."
+            />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>
+                Some representative placeholder content for the second slide.
+              </p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="img/about-hero.jpg" class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>
+                Some representative placeholder content for the third slide.
+              </p>
+            </div>
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </section>
+
+    <section class="priceSection">
+      <div class="container">
+        <div class="text-center">
+          <h2>
+            Please inquire today to get a personalized quote and discover our
+            reasonable rates!
+          </h2>
+          <div class="container">
+            <div class="row">
+              <div class="col-sm column1 pb-5">
+                <div class="img">
+                  <img src="img/in-touch-lg.png" alt="" />
+                </div>
+                <p class="buttonTagP text-white pt-4 pb-4">
+                  Inquire online and we will be in-touch within 24 hours
+                </p>
+                <button
+                  type="button"
+                  class="btn btn-outline-primary text-white"
+                >
+                  INQUIRE NOW
+                </button>
+              </div>
+
+              <div class="col-sm column1 pb-5">
+                <div class="img">
+                  <img src="img/download-info-lg.png" alt="" />
+                </div>
+                <p class="buttonTagP text-white pt-4 pb-4">
+                  Download our Information Package to learn more
+                </p>
+                <button
+                  type="button"
+                  class="btn btn-outline-primary text-white"
+                >
+                  DOWNLOAD
+                </button>
+              </div>
+
+              <div class="col-sm column1 pb-5">
+                <div class="img">
+                  <img src="img/in-touch-lg.png" alt="" />
+                </div>
+                <p class="buttonTagP text-white pt-4 pb-4">
+                  Ready to have your property 5 Star Professionally Managed?
+                </p>
+                <button
+                  type="button"
+                  class="btn btn-outline-primary text-white"
+                >
+                  GET STARTED
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="followSec">
+      <div class="container p-5">
+        <div class="row">
+          <div class="col-sm-6 leftColumn">
+            <h1>Follow Us</h1>
+          </div>
+          <div class="col-sm-6">
+            <img src="img/instagram-icon.png" alt="" />
+          </div>
+        </div>
+        <div class="ulDiv">
+          <ul class="pictureUl">
+            <li class="pictureLi"><img src="img/home1.jpg" alt="" /></li>
+            <li class="pictureLi"><img src="img/home2.jpg" alt="" /></li>
+            <li class="pictureLi"><img src="img/for rent.jpg" alt="" /></li>
+            <li class="pictureLi"><img src="img/smily.jpg" alt="" /></li>
+            <li class="pictureLi"><img src="img/88.jpg" alt="" /></li>
+          </ul>
+
+          <button type="button" class="btn btn-outline-primary text-white">
+            GET STARTED
+          </button>
+        </div>
+      </div>
+    </section>
+    <?php require 'footer.php'; ?>
+
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
